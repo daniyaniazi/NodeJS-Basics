339c08af279778401d6cc5e7ad5688c40f52403b
daniyaniazi <farihadania@hotmail.com>
2021-03-28 21:38:44 +0500

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -4,10 +4,17 @@ const port = 3000;
 
 //Creating server
 const server = http.createServer((request, response) => {
-    response.statusCode = 200; //OK
-    response.setHeader("Content-type", 'text/plain')
+    response.writeHead(200, { "Content-type": 'text/plain' },)
+    //response.statusCode = 200; //OK
+    //.setHeader("Content-type", 'text/plain')
+
+
     //return data to request
-    response.end("welcome to HTTP server")
+    response.write("Welcome To HTTP Server")
+    response.end()
+    //-----------------------------
+    //response.end("welcome to HTTP server")
+
 })
 
 server.listen(port, hostname, () => {
